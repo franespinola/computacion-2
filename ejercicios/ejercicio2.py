83aaca22b9307bef871c13f151e143e0f21b4546
franespinola <franespinola_92@hotmail.com>
2022-03-28 18:29:54 -0300

    ejercicio2 falta terminar

--- ejercicios/ejercicio2.py
+++ ejercicios/ejercicio2.py
@@ -1,9 +1,46 @@
 import argparse
+from asyncore import write
+from io import open
+import os
+def main():
+    parser = argparse.ArgumentParser(description='Ingresar nombre de los archivos.')
 
-parser = argparse.ArgumentParser(description="ejemplo parser")
+    parser.add_argument("-f", "--file1", type=str, help="archivo 1")
+    parser.add_argument("-s", "--file2", type=str, help="archivo 2")
+    args = parser.parse_args()
 
-parser.add_argument("-i", "--file", type=str, required=False, help="string")
-parser.add_argument("-o", "--size", type=int, default=1024, help="numero")
-args = parser.parse_args()
+    print(args.file1, args.file2) #muestro los argumentos
+
+    file1=args.file1
+    file2=args.file2
+
+
+    if os.path.isfile(file1):
+        with open(file1,'rw') as file:
+            file.write(file2)
+        exit()
+    else:
+        archivo_texto= open(file1,"w")
+        archivo_texto.write(file2)
+        archivo_texto.close()
+    #print(texto)
+
+'''
+archivo_texto= open(file1,"w")
+archivo_texto.write(file2)
+archivo_texto.close()
+'''
+
+''''
+try:
+    file=open(args.file1)
+    print(file)
+    file.close()
+except FileNotFoundError:
+    print('el archivo no existe')
+    exit()
+'''
+
+main()
 
 
